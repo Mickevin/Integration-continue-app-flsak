aeb4235213ae6e1b9cd22a44695bfe354137793e
Kévin Duranty <duranty.kevin@gmail.com>
2022-11-09 12:49:27 +0100

    last version stable

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 from flask import Flask, render_template, request
+import requests
 import warnings
+import time
+
+# Données à afficher
+#Adresses IP de la semabox / Nom de la semabox
+#o IP publique de l’accès Internet / nom dns dynamique
+#o Etat de la connexion Internet
+#o Liste des machines détectées sur le réseau
+#o Résultats du dernier test de débit
 
 warnings.filterwarnings('ignore')
-import socket
 
+def date_time():
+    day = time.localtime().tm_mday
+    month = time.localtime().tm_mon
+    year = time.localtime().tm_year
+    
+    return f'{day}/{month}/{year}'
 
-name = socket.gethostname().split('.')[0]
-
+date = date_time()
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def app_home():
-    
-    return render_template("index.html", name=name)
+    return render_template("index.html", date=date)
 
 
 @app.route('/submit/', methods=['POST'])
 def result():    
     if request.method == 'POST':
-        print(request.form.get('name'))
-        print(request.form.get('email'))
         return render_template("submit.html")
 
 
